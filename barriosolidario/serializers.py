7474386e8f7fcb6e617c9a9d6f9ca8d0afbc2ea1
Thibault Metais <thibault.metais31@gmail.com>
2019-11-28 15:26:46 -0300

    News serializer modified : username added

--- barriosolidario/serializers.py
+++ barriosolidario/serializers.py
@@ -8,15 +8,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 class NewsPostSerializer(serializers.ModelSerializer):
     from_me = serializers.SerializerMethodField('is_me')
+    username = serializers.SerializerMethodField('get_username')
 
     def is_me(self, post):
         #return post.user.id == self.context['request'].user.id
         # We don't have login feature right now (AnonymousUser used), so we are using a temporary solution
         return str(post.user.id) == "9781200a-4862-4daa-9231-a759c28d7ff7"
 
+    def get_username(self, post):
+        return User.objects.get(id=post.user.id).username
+
     class Meta:
         model = NewsPost
-        fields = ('id', 'content', 'picture', 'location', 'user', 'from_me')
+        fields = ('id', 'content', 'picture', 'location', 'user', 'from_me', 'username')
 
 class HelpPostSerializer(serializers.ModelSerializer):
     class Meta:
